f6d843510592bc0b808cbdc1dc9d594251c6a643
Argyn Khuan <argynkhuan@yandex.kz>
2020-12-25 23:15:42 +0600

    match fix

--- src/feature_matching.py
+++ src/feature_matching.py
@@ -1,26 +1,31 @@
-import cv2
 import numpy as np
+import cv2 as cv
 import matplotlib.pyplot as plt
 
-img1 = cv2.imread("res/set-1/sift-scene.jpg", None)
-img2 = cv2.imread("res/set-1/object-1.jpg", None)
+img1 = cv.imread("res/set-1/scene.png", cv.IMREAD_GRAYSCALE) # queryImage
+img2 = cv.imread("res/set-1/object.png", cv.IMREAD_GRAYSCALE) # trainImage
 
-# ORB Detector
-orb = cv2.ORB_create()
+# Initiate ORB detector
+orb = cv.ORB_create()
+
+# find the keypoints and descriptors with ORB
 kp1, des1 = orb.detectAndCompute(img1, None)
 kp2, des2 = orb.detectAndCompute(img2, None)
 
-# Brute Force Matching
-bf = cv2.BFMatcher(cv2.NORM_L1, crossCheck=False)
+# create BFMatcher object
+bf = cv.BFMatcher(cv.NORM_HAMMING, crossCheck=True)
 
+# Match descriptors.
 matches = bf.match(des1, des2)
 
+# Sort them in the order of their distance.
 matches = sorted(matches, key = lambda x:x.distance)
 
-matching_result = cv2.drawMatches(img1, kp1, img2, kp2, matches[:50], None, flags=cv2.DrawMatchesFlags_NOT_DRAW_SINGLE_POINTS)
+# Draw first 10 matches.
+img3 = cv.drawMatches(img1, kp1, img2, kp2, matches[:15], None, flags=cv.DrawMatchesFlags_NOT_DRAW_SINGLE_POINTS)
+# plt.imshow(img3),plt.show()
 
-# cv2.imshow("Img1", img1)
-# cv2.imshow("Img2", img2)
-cv2.imshow("Matching result", matching_result)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
+# cv image show
+cv.imshow("Matching result", img3)
+cv.waitKey(0)
+cv.destroyAllWindows()
