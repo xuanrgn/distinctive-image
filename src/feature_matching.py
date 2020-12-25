5c5b61e8b7f76db80fd62859d66af8bc90d95db8
Argyn Khuan <argynkhuan@yandex.kz>
2020-12-25 14:55:23 +0600

    path fix

--- src/feature_matching.py
+++ src/feature_matching.py
@@ -1,8 +1,9 @@
 import cv2
 import numpy as np
+import matplotlib.pyplot as plt
 
-img1 = cv2.imread("res/set-1/sift-scene.jpg", cv2.IMREAD_GRAYSCALE)
-img2 = cv2.imread("res/set-2/object-4.jpg", cv2.IMREAD_GRAYSCALE)
+img1 = cv2.imread("res/set-1/sift-scene.jpg", None)
+img2 = cv2.imread("res/set-1/object-1.jpg", None)
 
 # ORB Detector
 orb = cv2.ORB_create()
@@ -10,11 +11,13 @@ kp1, des1 = orb.detectAndCompute(img1, None)
 kp2, des2 = orb.detectAndCompute(img2, None)
 
 # Brute Force Matching
-bf = cv2.BFMatcher(cv2.NORM_HAMMING, crossCheck=True)
+bf = cv2.BFMatcher(cv2.NORM_L1, crossCheck=False)
+
 matches = bf.match(des1, des2)
+
 matches = sorted(matches, key = lambda x:x.distance)
 
-matching_result = cv2.drawMatches(img1, kp1, img2, kp2, matches[:50], None, flags=2)
+matching_result = cv2.drawMatches(img1, kp1, img2, kp2, matches[:50], None, flags=cv2.DrawMatchesFlags_NOT_DRAW_SINGLE_POINTS)
 
 # cv2.imshow("Img1", img1)
 # cv2.imshow("Img2", img2)
